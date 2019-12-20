f9158a3923da536c1fab0060d58851b9d13852fd
Victor Vuelma <victorvuelma@outlook.com>
2019-12-20 00:07:59 -0300

    setup: add devmenu access with 3 screen touchs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import DevMenu from '@terrysahaidak/react-native-devmenu';
+
 import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
@@ -12,7 +14,9 @@ export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <View />
+        <DevMenu>
+          <View />
+        </DevMenu>
       </PersistGate>
     </Provider>
   );
